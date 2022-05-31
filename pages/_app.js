89c1e050b9ceb614ee797efe72b5f1fb1715b94f
Felds Liscia <dev@felds.com.br>
2022-05-30 21:42:20 -0300

    add exitBeforeEnter

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AnimatePresence>
+    <AnimatePresence exitBeforeEnter>
       <Component {...pageProps} />
     </AnimatePresence>
   );
